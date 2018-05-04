44ee856c6cac78a8b6974c23bd7bf405e0315b0e
Aiyi Zhang <aiyizhang@gmail.com>
2018-05-03 23:48:30 -0400

    Update 2_cleaning med data initially.R

--- 2_cleaning med data initially.R
+++ 2_cleaning med data initially.R
@@ -1,4 +1,4 @@
-#cleanning diab and aspirin data initial 
+#cleanning diab data initial (identify med code in prescription)
 require(readr)
 library(readxl)
 require(lubridate)
@@ -6,10 +6,6 @@ library(reshape2)
 require(dplyr)
 #set work directory and read in data
 setwd("~/Macabi/data_generated/method9.3")
-
-#sample data
-#sample <- read_csv("~/Macabi/data_generated/method9.2/sample.csv")
-#patients_data <- read_csv("~/Macabi/Macabi_raw/patients_data.csv")
 
 #medicatino data
 med <- read_csv("~/Macabi/Macabi_raw/04 - Medication purchases.csv")
@@ -21,38 +17,6 @@ med_code <- read_excel("~/Macabi/Macabi_raw/04A - Medication codes description (
 #clean med date 
 med$medDate=ymd(med$Date)
 med$Date=NULL
-
-# #find all aspirin
-# aspirin_where_1=which(med_code$Treatment_Group=="ASA")
-# aspirin_where_2=grep("aspirin", tolower(med_code$Ingridients))
-# aspirin_where=unique(c(aspirin_where_1, aspirin_where_2))
-# 
-# #put aspirin into one column
-# med_code$aspirinInd=rep(0, dim(med_code)[1])
-# med_code$aspirinInd[aspirin_where]=1
-# 
-# #find the largo of aspirin med
-# aspirin_largo=unique(med_code$LARGO[med_code$aspirinInd==1])
-# 
-# med$aspirinInd=rep("non aspirin",dim(med)[1])
-# med$aspirinInd[which( med$LARGO %in% aspirin_largo)]="aspirin"
-# 
-# aspirin=med[which(med$aspirinInd=="aspirin"),]
-# vars=c("RANDOM_ID","DDD","medDate" )
-# aspirin1=aspirin[vars]
-# write.csv(aspirin1,"aspirinInit.csv")
-
-# #merge asp med with patients data by random id
-# varAsp=c("RANDOM_ID","aspirinInd")
-# med_asp=unique(med[,varAsp])
-# med_asp1=dcast(med_asp, RANDOM_ID~aspirinInd)
-# med_asp1$aspirin[is.na(med_asp1$aspirin)]="nonAsp"
-# med_asp1$`non aspirin`=NULL
-# med_asp2=med_asp1[which(med_asp1$aspirin=="aspirin"),]
-# asp_ppl=unique(med_asp2$RANDOM_ID)
-# sample$asprin=rep(0,dim(sample)[1])
-# sample$asprin[which(sample$RANDOM_ID %in% asp_ppl )]=1
-
 
 
 #diab med 
